4889033a86b48062856197673e4b51cdf1fc8767
ishii takaya <ishii.takaya.x@gmail.com>
2016-06-11 00:15:18 +0900

    yes/no文字列の場合もtrue/falseに変換するパターンを追加

--- src/test/java/modelmapper/sample/BooleanToStringTest.java
+++ src/test/java/modelmapper/sample/BooleanToStringTest.java
@@ -1,6 +1,7 @@
 package modelmapper.sample;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
@@ -8,16 +9,23 @@ import org.junit.Test;
 public class BooleanToStringTest {
 
 	@Test
-	public void 真の場合は文字列trueに変換() {
+	public void trueの場合は文字列trueに変換() {
 		SrcBooleanToString src = new SrcBooleanToString(true);
 		DestBooleanToString dest = BooleanToString.convert(src);
 		assertThat(dest.getFlag(), is("true"));
 	}
 
 	@Test
-	public void 偽の場合は文字列falseに変換() {
+	public void falseの場合は文字列falseに変換() {
 		SrcBooleanToString src = new SrcBooleanToString(false);
 		DestBooleanToString dest = BooleanToString.convert(src);
 		assertThat(dest.getFlag(), is("false"));
+	}
+	
+	@Test
+	public void nullの場合はnullのまま() {
+		SrcBooleanToString src = new SrcBooleanToString(null);
+		DestBooleanToString dest = BooleanToString.convert(src);
+		assertNull(dest.getFlag());
 	}
 }
